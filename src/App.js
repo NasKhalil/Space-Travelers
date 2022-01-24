ff48a1f5c6bae322124675b569542e255561d6ec
emmanuel <gbengacode>
2022-01-24 16:40:35 +0100

    Add route and view with active link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MyProfile from './pages/MyProfile';
-import './App.css';
+import Rockets from './pages/Rockets';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/profile" element={<MyProfile />} />
-      </Routes>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes>
+      <Route path="/profile" element={<MyProfile />} />
+      <Route path="/rockets" element={<Rockets />} />
+    </Routes>
+  </div>
+);
 
 export default App;
