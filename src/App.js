2c4d4419e6ec2a2d953e67d5310c73452cfd2a5c
Khalil <khalilnasri92@gmail.com>
2022-01-25 11:13:26 +0100

    fix linters errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MyProfile from './pages/MyProfile';
 import Rockets from './pages/Rockets';
-import Missions from './pages/missions';
+import Missions from './pages/Missions';
 
 const App = () => (
   <div className="App">
