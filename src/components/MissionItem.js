a2df4ec7913dc81ee1361171fdb0d8d348a6d11a
Khalil <khalilnasri92@gmail.com>
2022-01-25 10:38:04 +0100

    fix linters errors

--- src/components/MissionItem.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const MissionItem = () => {
-  return <div>mission item</div>;
-};
--- src/pages/Missions.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const missions = () => {
-  return <div>missions</div>;
-};
-
-export default missions;
