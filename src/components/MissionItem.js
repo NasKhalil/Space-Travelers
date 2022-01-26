939f51ffa1b9f5edad12b23372db4efd9582df0f
Khalil <khalilnasri92@gmail.com>
2022-01-26 10:21:23 +0100

    render missions list

--- src/components/MissionItem.js
+++ src/components/MissionItem.js
@@ -1,23 +1,24 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
+import { Button, Badge } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 const MissionItem = (props) => {
-  const { name } = props;
+  const { name, description } = props;
   return (
-    <tr>
-      <td>
-        {' '}
-        {name}
-        {' '}
-      </td>
-    </tr>
+    <>
+      <td>{name}</td>
+      <td>{ description }</td>
+      <td><Badge bg="secondary">Not A Member</Badge></td>
+      <td><Button variant="secondary" type="button">Join Mission</Button></td>
+    </>
   );
 };
 
 MissionItem.prototypes = {
   name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default MissionItem;
