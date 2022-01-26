8d8a946442d48fd7b8cdf1db1621a4b16aaeb86a
Khalil <khalilnasri92@gmail.com>
2022-01-26 11:57:27 +0100

    added missions actions and switch badges

--- src/components/MissionItem.js
+++ src/components/MissionItem.js
@@ -3,22 +3,62 @@
 import React from 'react';
 import { Button, Badge } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission, leaveMission } from '../redux/missions/missions';
 
 const MissionItem = (props) => {
-  const { name, description } = props;
+  const {
+    id, name, description, reserved,
+  } = props;
+
+  const dispatch = useDispatch();
   return (
     <>
       <td>{name}</td>
       <td>{ description }</td>
-      <td><Badge bg="secondary">Not A Member</Badge></td>
-      <td><Button variant="secondary" type="button">Join Mission</Button></td>
+      <td>
+        {
+          reserved
+            ? <Badge bg="info">Active Member</Badge>
+            : <Badge bg="secondary">Not A Member</Badge>
+        }
+      </td>
+      <td>
+        {
+          reserved
+            ? (
+              <Button
+                type="button"
+                variant="outline-danger"
+                onClick={() => dispatch(leaveMission(id))}
+              >
+                Leave Mission
+              </Button>
+            )
+            : (
+              <Button
+                type="button"
+                variant="outline-secondary"
+                onClick={() => dispatch(joinMission(id))}
+              >
+                Join Mission
+              </Button>
+            )
+        }
+      </td>
     </>
   );
 };
 
 MissionItem.prototypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  reserved: PropTypes.bool,
+};
+
+MissionItem.defaultProps = {
+  reserved: false,
 };
 
 export default MissionItem;
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,9 +1,21 @@
 const FETCH_MISSION = 'missions/FETCH_MISSION';
+const JOIN_MISSION = 'missions/JOIN_MISSION';
+const LEAVE_MISSION = 'missions/LEAVE_MISSION';
 
 const missionApiUrl = 'https://api.spacexdata.com/v3/missions';
 
 export const fetchMission = (payload) => ({
   type: FETCH_MISSION,
+  payload,
+});
+
+export const joinMission = (payload) => ({
+  type: JOIN_MISSION,
+  payload,
+});
+
+export const leaveMission = (payload) => ({
+  type: LEAVE_MISSION,
   payload,
 });
 
@@ -33,6 +45,21 @@ const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MISSION:
       return [...action.payload];
+
+    case JOIN_MISSION: {
+      const newState = state.map((mission) => {
+        if (mission.id !== action.payload) return mission;
+        return { ...mission, reserved: true };
+      });
+      return newState;
+    }
+    case LEAVE_MISSION: {
+      const newState = state.map((mission) => {
+        if (mission.id !== action.payload) return mission;
+        return { ...mission, reserved: false };
+      });
+      return newState;
+    }
     default:
       return state;
   }
